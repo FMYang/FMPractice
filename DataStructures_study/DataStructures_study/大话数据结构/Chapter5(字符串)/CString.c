f1d3af53b1dbda9f106dc3e7aa7be459e5b75488
yangfangming <451493485@qq.com>
2022-09-26 18:44:06 +0800

    -

--- DataStructures_study/DataStructures_study/大话数据结构/Chapter5(字符串)/CString.c
+++ DataStructures_study/DataStructures_study/大话数据结构/Chapter5(字符串)/CString.c
@@ -15,31 +15,8 @@
  则返回第一个这样的子串在S中位置，否则返回0
  */
 
-// KMP算法
-void get_nextVal(char *T, int *nextVal) {
-    int len = (int)strlen(T);
-    int i, j;
-    i = 1;
-    j = 0;
-    nextVal[0] = 1;
-    nextVal[1] = 0;
-    while (i < len) {
-        if(j == 0 && T[i] == T[j]) {
-            ++i;
-            ++j;
-            if(T[i] != T[j]) {
-                nextVal[i] = j;
-            } else {
-                nextVal[i] = nextVal[j];
-            }
-        } else {
-            j = nextVal[j];
-        }
-    }
-}
-
 // 朴素模式匹配算法
-int Index(char *S, char *T, int pos) {
+static int Index(char *S, char *T, int pos) {
     int lens = (int)strlen(S);
     int lent = (int)strlen(T);
     int i = pos; // 第i个位置下标为i
@@ -62,10 +39,64 @@ int Index(char *S, char *T, int pos) {
     }
 }
 
+// KMP算法
+static void KMP_GetNext(char *T, int *next) {
+    int lent = (int)strlen(T);
+    next[0] = 0;
+    int i = 1;
+    int j = 0;
+    while (i < lent) {
+        if(T[i] == T[j]) {
+            ++j;
+            next[i] = j;
+            ++i;
+        } else {
+            if(j == 0) {
+                next[i] = 0;
+                i++;
+            } else {
+                j = next[j - 1];
+            }
+        }
+    }
+}
+
+static int KMP_Index(char *S, char *T, int pos) {
+    int lens = (int)strlen(S);
+    int lent = (int)strlen(T);
+    int i = 0; // 第i个位置下标为i
+    int j = 0; // 子串开始遍历的下标
+    int next[lent];
+    KMP_GetNext(T, next);
+    while(i < lens) {
+        if(T[j] == S[i]) {
+            ++i;
+            ++j;
+            if(j == lent) {
+                // 全匹配
+                return i - j;
+            } else {
+                j = next[j - 1];
+            }
+        } else if(i < lens && T[j] != S[i]) {
+            // 不匹配
+            if(j > 0) {
+                j = next[j - 1];
+            } else {
+                ++i;
+            }
+        }
+    }
+    return -1;
+}
+
+
 void String_Example(void) {
     char *s = "ababaaabaa";
     char *t = "aa";
-    int r = Index(s, t, 1);
+//    char *tt = "ABCABCD";
+    int r = KMP_Index(s, t, 0);
+//    int r = Index(s, t, 0);
     if(r == -1) {
         printf("未找到子串\n");
     } else {
