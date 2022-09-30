02b7eeae8ba96d42535d7bd79db9e5983550f61a
yangfangming <451493485@qq.com>
2022-09-30 18:44:17 +0800

    -

--- DataStructures_study/DataStructures_study/大话数据结构/Chapter7(图)/Graph.c
+++ DataStructures_study/DataStructures_study/大话数据结构/Chapter7(图)/Graph.c
@@ -27,11 +27,12 @@
  在有向图中查找强连通分量
  检查给定的图是否本质上是二分图
  */
+
 #include "Graph.h"
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAXVERTEX 7
+#define MAXVERTEX 9
 
 ////////////////////////////////////////////////////
 typedef struct node {
@@ -58,16 +59,16 @@ static int EnQueue(LinkQueue *Q, int e) {
     return 1;
 }
 
-static int DeQueue(LinkQueue *Q) {
+static int DeQueue(LinkQueue *Q, int *e) {
     if(QueueIsEmpty(Q)) return 0;
     QNode *p = Q->front->next;
-    int e = p->data;
+    *e = p->data;
     Q->front->next = p->next;
     if(Q->rear == p) {
         Q->rear = Q->front;
     }
     free(p);
-    return e;
+    return 1;
 }
 
 static void Queue_Create(LinkQueue *Q) {
@@ -77,83 +78,104 @@ static void Queue_Create(LinkQueue *Q) {
 
 ////////////////////////////////////////////////////////
 typedef struct Graph {
-    int numberOfVertexs; // 顶点数
-    int vertex[MAXVERTEX]; // 顶点表
+    char vertex[MAXVERTEX]; // 顶点表
     int edge[MAXVERTEX][MAXVERTEX]; // 邻接矩阵
+    int numberOfVertexs; // 顶点数
 } Graph;
-
-static void CreateGraph(Graph *graph) {
-    for(int i=0; i<MAXVERTEX; i++) {
-        graph->vertex[i] = i;
-    }
-    for(int i=0; i<MAXVERTEX; i++) {
-        for(int j=0; j<MAXVERTEX; j++) {
-            graph->edge[i][j] = 0;
-        }
-    }
-}
 
 static void AddEdge(Graph *graph, int from, int to) {
     graph->edge[from][to] = 1;
     graph->edge[to][from] = 1;
 }
 
-void bfsTraversal(Graph *graph, int vertex) {
+static void CreateGraph(Graph *graph) {
+    graph->numberOfVertexs = 9;
+    for(int i=0; i<graph->numberOfVertexs; i++) {
+        for(int j=0; j<graph->numberOfVertexs; j++) {
+            graph->edge[i][j] = 0;
+        }
+    }
+    
+//    graph->vertex[0]='A';
+//    graph->vertex[1]='B';
+//    graph->vertex[2]='C';
+//    graph->vertex[3]='E';
+//    graph->vertex[4]='F';
+//
+//    AddEdge(graph, 0, 1);
+//    AddEdge(graph, 0, 2);
+//    AddEdge(graph, 1, 3);
+//    AddEdge(graph, 2, 4);
+//    AddEdge(graph, 3, 4);
+
+    
+    graph->vertex[0]='A';
+    graph->vertex[1]='B';
+    graph->vertex[2]='C';
+    graph->vertex[3]='D';
+    graph->vertex[4]='E';
+    graph->vertex[5]='F';
+    graph->vertex[6]='G';
+    graph->vertex[7]='H';
+    graph->vertex[8]='I';
+
+    AddEdge(graph, 0, 1);
+    AddEdge(graph, 0, 5);
+
+    AddEdge(graph, 1, 2);
+    AddEdge(graph, 1, 8);
+    AddEdge(graph, 1, 6);
+
+    AddEdge(graph, 2, 3);
+    AddEdge(graph, 2, 8);
+
+    AddEdge(graph, 3, 4);
+    AddEdge(graph, 3, 7);
+    AddEdge(graph, 3, 6);
+    AddEdge(graph, 3, 8);
+
+    AddEdge(graph, 4, 5);
+    AddEdge(graph, 4, 7);
+
+    AddEdge(graph, 5, 6);
+
+    AddEdge(graph, 6, 7);
+}
+
+// 广度优先遍历
+void BFSTraversal(Graph graph, int vertex) {
     bool visited[MAXVERTEX] = { false };
         
+    // 初始化一个辅助用的队列
     LinkQueue queue;
     Queue_Create(&queue);
     
-    for(int i = vertex; i < MAXVERTEX; i++) {
-        if(!visited[i]) {
-            visited[i] = true;
-            printf("%d ", graph->vertex[i]);
-            EnQueue(&queue, i);
-            while (!QueueIsEmpty(&queue)) {
-                i = DeQueue(&queue);
+    // 遍历所有顶点
+    for(int i = 0; i < graph.numberOfVertexs; i++) {
+        if(!visited[i]) { // 未访问过就处理
+            visited[i] = true; // 设置当前顶点访问过
+            printf("%c ", graph.vertex[i]);
+            EnQueue(&queue, i); // 将此顶点入队列
+            
+            while (!QueueIsEmpty(&queue)) { // 当前队列不为空
+                DeQueue(&queue, &i); // 将队列中的元素出列，赋值给i
                 
-                for(int j=0; j<MAXVERTEX; j++) {
-                    if(graph->edge[i][j] == 1 && !visited[j]) {
-                        visited[j] = true;
-                        printf("%d ", graph->vertex[j]);
-                        EnQueue(&queue, j);
+                for(int j=0; j<graph.numberOfVertexs; j++) {
+                    // 判断其他顶点若与当前顶点存在边，并未访问过
+                    if(graph.edge[i][j] == 1 && !visited[j]) {
+                        visited[j] = true; // 将找到的顶点标记为已访问
+                        printf("%c ", graph.vertex[j]);
+                        EnQueue(&queue, j); // 将找到的顶点入队列
                     }
                 }
             }
         }
     }
+    printf("\n");
 }
 
 void Graph_Example(void) {
-//    LinkQueue queue;
-//    Queue_Create(&queue);
-//    EnQueue(&queue, 1);
-//    EnQueue(&queue, 2);
-//    printf("出列: ");
-//    printf("%d ", DeQueue(&queue));
-//    printf("%d ", DeQueue(&queue));
-//    printf("%d ", DeQueue(&queue));
-//    printf("%d \n", DeQueue(&queue));
-    
     Graph graph;
     CreateGraph(&graph);
-    AddEdge(&graph, 0, 1);
-    AddEdge(&graph, 0, 2);
-    AddEdge(&graph, 1, 3);
-    AddEdge(&graph, 1, 4);
-    AddEdge(&graph, 2, 1);
-    AddEdge(&graph, 2, 5);
-    AddEdge(&graph, 3, 6);
-//
-//    //
-//    for(int i = 0; i<MAXVERTEX; i++) {
-//        for(int j = 0; j<MAXVERTEX; j++) {
-//            printf("[%d][%d] = %d ", i, j, graph.edge[i][j]);
-//            if(j > 0 && j % (MAXVERTEX - 1) == 0) {
-//                printf("\n");
-//            }
-//        }
-//    }
-//    printf("\n");
-    bfsTraversal(&graph, 1);
+    BFSTraversal(graph, 0);
 }
